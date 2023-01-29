f36a4e6b27771e1d52519aa71448496462062ca3
Nino Verde <nino.verde@leonardocompany.com>
2023-01-29 09:15:40 -0800

    modified the way to get urls connected to a x-operation

--- src/integrations/ctis.py
+++ src/integrations/ctis.py
@@ -1,5 +1,6 @@
 import requests
 
+
 class CTIS():
 
     headers = {}
@@ -10,21 +11,23 @@ class CTIS():
         self.CTIS_login(username, password)
 
     def do_req(self, url):
-        return requests.get(self.url + url, headers = self.headers).json()
-    
+        return requests.get(self.url + url, headers=self.headers).json()
+
     # Horrible, to enhance
     def get_operations_and_urls(self, op_label):
-        ops = self.do_req(f"/x-operations?where=%7B%22labels%22%3A%20%5B%22{op_label}%22%5D%7D")["_items"]
-        rels = self.do_req("/relationships?where=%7B%22source_type%22%3A%20%22x-operations%22%2C%20%22target_type%22%3A%20%22urls%22%7D")["_items"]
+        # TODO: add where _created gte today minus a configurable interval
+        ops = self.do_req(
+            f"/x-operations?where=%7B%22labels%22%3A%20%5B%22{op_label}%22%5D%7D")["_items"]
         res = {}
         for op in ops:
+            rels = self.do_req("/urls/relationships/x-operations/"+op['_id'])
             res[op["name"]] = []
-            for rel in rels:
-                if op["_id"] == rel["source_ref"]:
-                    res[op["name"]].append(self.do_req(f"/urls/{rel['target_ref']}")["value"])
+            for rel in rels['_items']:
+                res[op["name"]].append(rel['value'])
         return res
 
     def CTIS_login(self, user, password):
         #response = requests.post(f"{self.url}/api/auth/login", json={"username": user, "password": password})
         response = requests.get(f"{self.url}/login", auth=(user, password))
-        self.headers = {'accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + response.json()["data"]["access_token"]}
+        self.headers = {'accept': 'application/json', 'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + response.json()["data"]["access_token"]}
